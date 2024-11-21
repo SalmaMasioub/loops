ad2f1d490acf4503ba602703599bd6039d7b3081
SalmaMasioub <salma.masioub6@gmail.com>
2024-11-21 13:03:12 +0100

    first commit

---
+++ loopss.js
@@ -0,0 +1,17 @@
+
+for(i=1 ; i<30; i+=2
+
+){ 
+
+console.log(i)}
+
+
+
+let fruits= ["banana", "poivre", "fraise"]
+let upperFruits=[]
+for(
+     i=0; i<fruits.length; i++  
+){ 
+    upperFruits.push(fruits[i].toUpperCase())
+}
+console.log(upperFruits)
